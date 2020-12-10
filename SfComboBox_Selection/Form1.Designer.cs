11e52a855d00542452ce01c2e5228ff08224e7df
Backia Raj Kanagaraj <backiaraj.kanagaraj@syncfusion.com>
2020-12-10 12:31:26 +0530

    Added sample for how to select the item using index in WF SfComboBox

---
+++ SfComboBox_Selection/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿
+namespace SfComboBox_Selection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sfComboBox1 = new Syncfusion.WinForms.ListView.SfComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.sfComboBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sfComboBox1
+            // 
+            this.sfComboBox1.Location = new System.Drawing.Point(130, 56);
+            this.sfComboBox1.Name = "sfComboBox1";
+            this.sfComboBox1.Size = new System.Drawing.Size(150, 28);
+            this.sfComboBox1.Style.TokenStyle.CloseButtonBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.sfComboBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CaptionBarColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(158)))), ((int)(((byte)(218)))));
+            this.CaptionButtonColor = System.Drawing.Color.White;
+            this.CaptionForeColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(438, 264);
+            this.Controls.Add(this.sfComboBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SfComboBox_Index";
+            ((System.ComponentModel.ISupportInitialize)(this.sfComboBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Syncfusion.WinForms.ListView.SfComboBox sfComboBox1;
+    }
+}
+
---
+++ SfComboBox_Selection/Form1.cs
@@ -0,0 +1,75 @@
+﻿using Syncfusion.Windows.Forms;
+using Syncfusion.WinForms.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SfComboBox_Selection
+{
+    public partial class Form1 : MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            List<State> list = GetData();
+            sfComboBox1.DataSource = list;
+
+            //Bind the Display member and Value member to the data source
+            sfComboBox1.DisplayMember = "DispValue";
+            sfComboBox1.ValueMember = "SfValue";
+            sfComboBox1.ComboBoxMode = Syncfusion.WinForms.ListView.Enums.ComboBoxMode.SingleSelection;
+            sfComboBox1.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            sfComboBox1.AutoCompleteSuggestMode = Syncfusion.WinForms.ListView.Enums.AutoCompleteSuggestMode.Contains;
+            sfComboBox1.MaxDropDownItems = 10;
+
+            //Select the item based on index
+            sfComboBox1.SelectedIndex = list[1].SfValue;
+
+            //Select the item based on item
+            sfComboBox1.SelectedItem = list[1];
+        }
+
+        List<State> GetData()
+        {
+            List<State> states = new List<State>();
+            states.Add(new State("ASTM A 351 GR.CF3M", 2));
+            states.Add(new State("ASTM A 351 GR.CF8M", 3));
+            states.Add(new State("ASTM A 351 GR.CF8", 4));
+            states.Add(new State("ASTM A 351 GR.CF3", 5));
+            states.Add(new State("ASTM A 351 GR.CF3MN", 6));
+            states.Add(new State("ASTM A 351 GR.CF8C", 7));
+            states.Add(new State("ASTM A 351 GR.CN7M", 8));
+            states.Add(new State("ASTM A 351 GR.CN3MN", 9));
+            states.Add(new State("ASTM A 351 GR.CD4MCu", 10));
+            return states;
+        }
+
+        public class State
+        {
+            private int sfValue;
+            private object dispValue;
+
+            public State(object DispValue, int SfValue)
+            {
+                this.dispValue = DispValue;
+                this.sfValue = SfValue;
+            }
+
+            public int SfValue
+            {
+                get { return sfValue; }
+            }
+
+            public object DispValue
+            {
+                get { return dispValue; }
+            }
+        }
+    }
+}
